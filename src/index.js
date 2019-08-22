2fbfd54ed1f763035a20a03d96b9064ec5341b28
IC-Tech <ic.imesh.chamara@gmail.com>
2019-08-22 15:04:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* Copyright © Imesh Chamara 2019 */
 import './style.scss'
 import './icApp.js'
 import IAC from './icApp-creator.js'
@@ -5,11 +6,53 @@ import IAC from './icApp-creator.js'
 ic.init = icApp => {
 	var _root_ = new icApp.e('#root')
 	_root_.chr()
+
+	var user = {
+		name: 'Imesh Chamara',
+		bio: `I' m a Student and Programmer.
+Live in Sri Lanka.Doing coding stuff all the day with no purpose.
+Favorite things: Programming, Japanese-Music, Japan.
+
+I Programmed this Website.
+This is one of my website.
+
+http://ic-tech.dx.am/`,
+		image: ''
+	}
+
+var idCounter = 0 
+const icons = [
+'<svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z"></path></svg>',
+'<svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z"></path></svg>',
+'<svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="currentColor" d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm0 96c48.6 0 88 39.4 88 88s-39.4 88-88 88-88-39.4-88-88 39.4-88 88-88zm0 344c-58.7 0-111.3-26.6-146.5-68.2 18.8-35.4 55.6-59.8 98.5-59.8 2.4 0 4.8.4 7.1 1.1 13 4.2 26.6 6.9 40.9 6.9 14.3 0 28-2.7 40.9-6.9 2.3-.7 4.7-1.1 7.1-1.1 42.9 0 79.7 24.4 98.5 59.8C359.3 421.4 306.7 448 248 448z"></path></svg>',
+'<svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M402.6 83.2l90.2 90.2c3.8 3.8 3.8 10 0 13.8L274.4 405.6l-92.8 10.3c-12.4 1.4-22.9-9.1-21.5-21.5l10.3-92.8L388.8 83.2c3.8-3.8 10-3.8 13.8 0zm162-22.9l-48.8-48.8c-15.2-15.2-39.9-15.2-55.2 0l-35.4 35.4c-3.8 3.8-3.8 10 0 13.8l90.2 90.2c3.8 3.8 10 3.8 13.8 0l35.4-35.4c15.2-15.3 15.2-40 0-55.2zM384 346.2V448H64V128h229.8c3.2 0 6.2-1.3 8.5-3.5l40-40c7.6-7.6 2.2-20.5-8.5-20.5H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V306.2c0-10.7-12.9-16-20.5-8.5l-40 40c-2.2 2.3-3.5 5.3-3.5 8.5z"></path></svg>',
+'<svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M497 273L329 441c-15 15-41 4.5-41-17v-96H152c-13.3 0-24-10.7-24-24v-96c0-13.3 10.7-24 24-24h136V88c0-21.4 25.9-32 41-17l168 168c9.3 9.4 9.3 24.6 0 34zM192 436v-40c0-6.6-5.4-12-12-12H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h84c6.6 0 12-5.4 12-12V76c0-6.6-5.4-12-12-12H96c-53 0-96 43-96 96v192c0 53 43 96 96 96h84c6.6 0 12-5.4 12-12z"></path></svg>'
+]
+
 	IAC.create(icApp, _root_, [
-		{
-			t: 'div',
-			cl: 'Main',
-			ch: []
-		}
+		{ t: 'div', cl: ['ICApp', 'Main'], ch: [
+			{ t: 'div', ch: [
+				{ t: 'div', cl: 'c1' },
+				{ t: 'div', cl: 'c2', ch: [
+					{ t: 'span', cl: 'c1', txt: user.name },
+					{ t: 'span', cl: 'c3', txt: user.bio }
+				]},
+				{ t: 'label', at: [['for', 'ic_i'+(idCounter++)]], ch: [
+					{ t: 'input', at: [['type','checkbox'], ['id', 'ic_i' + --idCounter]]},
+					{ t: 'div', cl: 'c1', ch: [
+						{ t: 'div' },
+						{ t: 'div' },
+						{ t: 'div' }
+					]},
+					{ t: 'div', cl: 'c2', ch: [
+						{ t: 'a', at: [['href', '/']], html: icons[0] },
+						{ t: 'a', e: [['click', a => console.log(a)]], html: icons[1] },
+						{ t: 'a', e: [['click', a => console.log(a)]], html: icons[2] },
+						{ t: 'a', e: [['click', a => console.log(a)]], html: icons[3] },
+						{ t: 'a', e: [['click', a => console.log(a)]], html: icons[4] }
+					]}
+				]},
+			]}
+		]}
 	])
 }
