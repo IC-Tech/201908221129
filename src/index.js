a31fc590c9268990d6ef5beb5e389753278b95d6
IC-Tech <ic.imesh.chamara@gmail.com>
2019-08-22 14:05:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './style.scss'
+import './icApp.js'
+import IAC from './icApp-creator.js'
+
+ic.init = icApp => {
+	var _root_ = new icApp.e('#root')
+	_root_.chr()
+	IAC.create(icApp, _root_, [
+		{
+			t: 'div',
+			cl: 'Main',
+			ch: []
+		}
+	])
+}
