baa3768c13709b78c47cca315e60ec66075151c3
IC-Tech <ic.imesh.chamara@gmail.com>
2019-08-24 02:33:42 +0530

    Prevent from data leakage

--- src/icApp-updater.js
+++ src/icApp-updater.js
@@ -1,16 +1,16 @@
-if(!window.ic) {
-	window.ic = { icApp: {}}
-}
-window.ic.icApp.updater = {a: [],b:[]}
-class _a{
-static get a() {
-return window.ic.icApp.updater
-}
-static set a(v) {
-window.ic.icApp.updater = v
-}
+class _a {
+	constructor() {
+		this.updater = {a: [],b:[]}
+	}
+	static get a() {
+		return icAppUpdater.a.updater
+	}
+	static set a(v) {
+		icAppUpdater.a.updater = v
+	}
 }
 const icAppUpdater = {
+	a: new _a(),
 	link: (a, b) => ([a = [a, _a.a.a.indexOf(a)], _a.a.a[a[1] = a[1] == -1 ? _a.a.a.length : a[1]] = a[0], _a.a.b[a[1]] ? _a.a.b[a[1]].push(b) : _a.a.b[a[1]] = [b], a[1] +','+ (_a.a.b[a[1]].length - 1)])[3],
 	update: (a, b) => (a = _a.a.a.indexOf(a)) != -1 ? _a.a.b[a].forEach((c,d)=> c({i:[a,d], e: new ic.icApp.e(`[data-iau="${a},${d}"]`), v:b})) : 0
 }
