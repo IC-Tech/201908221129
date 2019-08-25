6c26a3f525bbd23035b95d83cc718baedef6de94
IC-Tech <ic.imesh.chamara@gmail.com>
2019-08-26 04:13:21 +0530

    Add update Events

--- src/icApp-updater.js
+++ src/icApp-updater.js
@@ -12,6 +12,8 @@ class _a {
 const icAppUpdater = {
 	a: new _a(),
 	link: (a, b) => ([a = [a, _a.a.a.indexOf(a)], _a.a.a[a[1] = a[1] == -1 ? _a.a.a.length : a[1]] = a[0], _a.a.b[a[1]] ? _a.a.b[a[1]].push(b) : _a.a.b[a[1]] = [b], a[1] +','+ (_a.a.b[a[1]].length - 1)])[3],
-	update: (a, b) => (a = _a.a.a.indexOf(a)) != -1 ? _a.a.b[a].forEach((c,d)=> c({i:[a,d], e: new ic.icApp.e(`[data-iau="${a},${d}"]`), v:b})) : 0
+	update: (a, b) => (a = _a.a.a.indexOf(a)) != -1 && (icAppUpdater.willUpdate ? icAppUpdater.willUpdate(a,b) != false : true) ? [_a.a.b[a].forEach((c,d)=> c({i:[a,d], e: new ic.icApp.e(`[data-iau="${a},${d}"]`), v:b})), icAppUpdater.didUpdate ? icAppUpdater.didUpdate() : 0] : 0,
+	didUpdate: null,
+	willUpdate: null
 }
 export default icAppUpdater
