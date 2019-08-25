ee8b0afb94961d23ec6917e0fec1a02a8b20d739
IC-Tech <ic.imesh.chamara@gmail.com>
2019-08-26 05:10:13 +0530

    Update common.js

--- src/common.js
+++ src/common.js
@@ -1,20 +1,6 @@
-function loadRoot() {
-  var str = document.URL;
-  var pos = 0;
-  var ind = str.indexOf('/', pos);
-  while (ind < str.length && ind >= 0) {
-    pos = ind + 1;
-    if (str.substr(ind - 1, 1) != ':' && str.substr(ind - 1, 1) != '/') {
-      str = str.substr(0, ind) + '/';
-      ind = -1;
-    }
-    else  {ind = str.indexOf('/', pos);}
-  }
-  return str;
-}
-
+const Host = a => location.origin + '/'
 const ReloadError = (e, f) => (f ? f : alert)(e)
-const XHR = (url, call, err, data) => {
+const XHR = (url, call, op, data) => {
    // eslint-disable-next-line no-undef
   var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP')
   xhr.open(data ? 'POST' : 'GET', url + ((url.indexOf('?') >= 0 ? '&' : '?') + 't=' + new Date().getTime()))
@@ -22,17 +8,17 @@ const XHR = (url, call, err, data) => {
     if (xhr.readyState == 4 && xhr.status != 0) {
       if(xhr.response) {
         try {
-          call(JSON.parse(xhr.response))
+          call(op && op.raw ? xhr.response : JSON.parse(xhr.response))
         } catch(e) {
-          ReloadError('Server response error. (EC: 0xA2 >>> ' + e + ' <)', err)
+          ReloadError('Server response error. (EC: 0xA2 >>> ' + e + ' <)', op && op.err ? op.err : undefined)
         }
       }
       else
-        ReloadError('Server response error. (EC: 0xA1)', err)
+        ReloadError('Server response error. (EC: 0xA1)', op && op.err ? op.err : undefined)
     }
   }
   xhr.onerror = function (e) {
-    if(e.target.status == 0) ReloadError("The Webpage can't connect to the server. Try again in a few moments.", err)
+    if(e.target.status == 0) ReloadError("The Webpage can't connect to the server. Try again in a few moments.", op && op.err ? op.err : undefined)
   }
   xhr.send(data)
 }
@@ -44,4 +30,4 @@ const pram = (a, b) => {
 }
 const mobile = a => ([a = [navigator.userAgent.toLowerCase(), 0], ['android','webos','iphone','ipad','ipod','blackberry','iemobile','opera mini'].forEach(b=> a[1] == 0 && a[0].indexOf(b) >= 0 ? (a[1] = 1) : 0)])[0][1] == 1
 
-export {XHR, pram, mobile}
+export {XHR, pram, mobile, Host}
