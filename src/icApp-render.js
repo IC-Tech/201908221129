6247cd92773ffcb25687159723f69f5cb66c3f45
IC-Tech <ic.imesh.chamara@gmail.com>
2019-08-27 18:49:45 +0530

    Update icApp-render.js

--- src/icApp-render.js
+++ src/icApp-render.js
@@ -12,11 +12,12 @@ const _elm = a => [
 a.d.t ? [a.e.v.tagName != a.d.t.toUpperCase() ? (a.e = new icApp.e(([a.t = icApp.cE(a.d.t), a.e.v.parentElement ? a.e.v.parentElement.replaceChild(a.e.v, a.t) : 0, a.t])[2])) : 0] : 0,
 a.d.s ? [_a([a.e.st, b => [a.e.st[b = a.e.st[b]] = a.d.s[b] ? a.d.s[b] : null, a.d.s[b] ? delete a.d.s[b] : 0]]), _a([a.d.s, b => a.e.st[b] = a.d.s[b]])] : 0,
 a.d.cl ? [a.d.cl = _b(a.d.cl), _a([a.e.cl, b => [(b = [b = a.e.cl[b], a.d.cl.indexOf(a.e.cl[b])])[1] == -1 ? a.e.clr(b[0]) : delete a.d.cl[b[0]]]]), _a([a.d.cl, b => a.e.cla(a.d.cl[b])])] : 0,
-a.d.at ? a.d.at.forEach((b,c) => a.e.v.getAttribute(b[0]) == b[1].toString() ? delete a.d.at[c] : a.e.v.setAttribute(b[0], b[1])) : 0,
+a.d.at ? a.d.at.forEach((b,c) => b[1] ? (a.e.v.getAttribute(b[0]) == b[1].toString() ? delete a.d.at[c] : a.e.v.setAttribute(b[0], b[1])) : a.e.v.removeAttribute(b[0])) : 0,
 a.d.e ? a.d.e.forEach(b => a.e.v[b[0]] = b[1]) : 0,
+a.d.d ? _a([a.d.d, b => a.e.d[b] = a.d.d[b]]) : 0,
 a.d.html ? [a.e.html != a.d.html ? a.e.html = a.d.html : 0] : 0,
 a.d.txt ? [a.e.txt != a.d.txt ? a.e.txt = a.d.txt : 0] : 0,
-a.d.ch ? [a.d.ch.forEach((b,c) => b ? (a.e.ch[c] ? _elm({e: new icApp.e(a.e.ch[c]), d: b}) : a.e.ap(_ce(b)[8].e.v)) : 0), a.e.ch.length > a.d.ch.length ? Loop(a.d.ch.length - a.e.ch.length, b => a.e.ch[a.d.ch.length + b].remove()) : 0] : 0,
+a.d.ch ? [a.d.ch.forEach((b,c) => b ? (a.e.ch[c] ? _elm({e: new icApp.e(a.e.ch[c]), d: b}) : a.e.ap(_ce(b)[9].e.v)) : 0), a.e.ch.length > a.d.ch.length ? Loop(a.d.ch.length - a.e.ch.length, b => a.e.ch[a.d.ch.length + b].remove()) : 0] : 0,
 a
 ]
 class icAppRender {
