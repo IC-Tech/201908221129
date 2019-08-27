fc96ab3a53c60c3969848605d7b4b157876ceb26
IC-Tech <ic.imesh.chamara@gmail.com>
2019-08-27 21:22:06 +0530

    Update signin.js

--- src/signin.js
+++ src/signin.js
@@ -2,9 +2,11 @@
 import './signin.scss'
 import './icApp.js'
 import './loading-ani.css'
+import './Dialog.css'
 import './inputui.scss'
 import {Theme} from './Theme.js'
 import {IAR} from './icApp-render.js'
+import {inputUI, dialogUI} from './IC-UI.js'
 import {XHR, Host} from './common.js'
 
 ic.init = icApp => {
@@ -13,15 +15,15 @@ var _root_ = new icApp.e('#root')
 _root_.chr()
 Theme.set('red')
 
-var inputUI = a => (
-	{t: 'div', cl: 'inputui', s: a.s, ch: [
-		{t:'label', at: [['for', a.id]], txt: a.name},
-		{t: a.multi ? 'textarea' : 'input', at: [['readonly', a.readonly], ['type', a.type], ['type', a.id], ['id', a.id]], e: [['onfocus', inputUI.check], ['onblur', inputUI.check], ['onchange', inputUI.check], ['oninput', inputUI.check]]}
-]})
-inputUI.check = a => new icApp.e(a = a.target).p[icApp.d.activeElement == a || a.value != '' ? 'cla' : 'clr']('s1')
 const stateMsg = [
-	`Enter your email to signin. If you don't have one, you can create new account by submit your email.`
+	`Enter your email to signin. If you don't have one, you can create new account by submit your email.`,
+	`Enter your password to signin. If this isn't your account click cancel button.`
 ]
+const ServerErr = a => dialogUI.create({name: 'Server Error', msg: `Error detected in server. ${a ? '' : 'No '}Error information${a ? (b => [a.forEach((a,c) => b += (c == 0 ? '' : ', ') + a), b])(': ')[1] : ' Provided'}.`, but: ['RELOAD', 'CANCEL'], f: a=> {
+	if(a.b == 0) location.reload()
+	else window.close()
+}})
+const defaultWait = 1200
 class ICTech extends IAR {
 	constructor() {
 		super()
@@ -29,13 +31,45 @@ class ICTech extends IAR {
 			UI: 0,
 			state: 0
 		}
+		this.submit = this.submit.bind(this)
+		this.cancel = this.cancel.bind(this)
 		setTimeout(async a=> {
 			this.update({UI: 1})
-		}, 1200)
+		}, defaultWait)
 	}
 	didMount() {
 	}
 	didUpdate() {
+		icApp.qsa('.inputui input').forEach(a=> inputUI.check({target: a}))
+	}
+	submit(e) {
+		e.preventDefault()
+		this.update({UI: 0})
+		const _a = a => a + '=' + icApp.qs('#' + a).value
+		setTimeout(a => {
+			if(this.data.state == 0)
+				XHR(API_Server + encodeURI('exists?' + _a('email')), a=> {
+					if(!a.success) return ServerErr(a.error)
+					this.update({state: a.response ? 1 : 2, UI: 1})
+				})
+			else if(this.data.state == 1)
+				XHR(API_Server + encodeURI('signin?' + _a('email') + '&' + _a('password')), a=> {
+					if(!a.success) return ServerErr(a.error)
+					localStorage.setItem('IC-Tech.User', JSON.stringify(a.response))
+					location = Host()
+				})
+			else if(this.data.state == 2)
+				XHR(API_Server + encodeURI('signup?' + _a('email') + '&' + _a('password') + '&' + _a('name')), a=> {
+					if(!a.success) return ServerErr(a.error)
+				})
+		}, defaultWait)
+		return false
+	}
+	cancel(e) {
+		e.preventDefault()
+		this.update({UI: 0})
+		setTimeout(a => this.update({state: 0, UI: 1}), defaultWait)
+		return false
 	}
 	render() {
 		return (
@@ -45,19 +79,21 @@ class ICTech extends IAR {
 				]},
 				{ t: 'div', cl: ['ICPage', 'Main'], s: {display: this.data.UI == 1 ? 'flex' : 'none'}, ch: [
 					{ t: 'div', ch: [
-						{ t: 'form', ch: [
+						{ t: 'form', e: [['onsubmit', this.submit]], ch: [
 							{ t: 'span', txt: 'Signin to IC-Tech' },
 							inputUI({id: 'email', type: 'email', name: 'Email', readonly: this.data.state != 0}),
 							inputUI({id: 'password', type: 'password', name: 'Password', s: {display: this.data.state != 0 ? 'block' : 'none'} }),
 							inputUI({id: 'name', type: 'name', name: 'Name', s: {display: this.data.state == 2 ? 'block' : 'none'}}),
-							inputUI({id: 'about', type: 'about', name: 'About', s: {display: this.data.state == 2 ? 'block' : 'none'}, multi: 1}),
 							{ t: 'span', cl: 'c1', s: {display: stateMsg[this.data.state] ? 'block' : 'none'}, txt: stateMsg[this.data.state] },
 							{ t: 'div', cl: 'c1', s: {paddingTop: '12px'}, ch: [
-								{ t: 'input', cl: ['ic-btn0', 's1'], at: [['type', 'submit'],['value', 'NEXT']]},
-								{ t: 'button', cl: 'ic-btn0', txt: 'CANCEL' }
+								{ t: 'input', cl: ['ic-btn0', 's1'], at: [['type', 'submit'], ['value', 'NEXT']]},
+								{ t: 'button', cl: 'ic-btn0', txt: 'CANCEL', e: [['onclick', this.cancel]], s: {display: this.data.state != 0 ? 'block' : 'none'} }
 							]}
 						]}
 					]}
+				]},
+				{ t: 'div', cl: ['ICPage', 'c1'], s: {display: this.data.UI == 2 ? 'flex' : 'none'}, ch: [
+					{ t:'span', txt: 'loading-ani' }
 				]}
 			]}
 		)
