e6485406319bb4a60356ec0c863d06918892c8cf
IC-Tech <ic.imesh.chamara@gmail.com>
2019-08-28 23:45:57 +0530

    Update verify.js

--- src/verify.js
+++ src/verify.js
@@ -4,7 +4,7 @@ import './icApp.js'
 import './loading-ani.css'
 import {Theme} from './Theme.js'
 import {IAR} from './icApp-render.js'
-import {XHR, Host, API, IC_DEV} from './common.js'
+import {XHR, Host, API, IC_DEV, pram} from './common.js'
 import {ShowErr} from './error.js'
 
 ic.init = icApp => {
@@ -15,13 +15,14 @@ Theme.set('red')
 const defaultWait = 1200
 var icons = null
 
-class SignIn extends IAR {
+class Verify extends IAR {
 	constructor() {
 		super()
 		this.data = {
 			UI: 0,
 			st: 0
 		}
+		if(IC_DEV) window.__IC_DEV__.Verify = this
 	}
 	didMount() {
 		var _a = new icApp.e('.load span')
@@ -39,7 +40,7 @@ class SignIn extends IAR {
 				return parseInt(a[0]) + ([' bytes', 'KB', 'MB', 'GB'])[a[1]]
 			}
 			var d = 0
-			const _icons = [15, 14]
+			const _icons = [15, 13]
 			for(var a=0; a<_icons.length; a++) {
 				if(!(icons[a] = localStorage.getItem('ic-tech:assets:v0:icon' + _icons[a]))) {
 					icons[a] = await b(_icons[a])
@@ -48,8 +49,10 @@ class SignIn extends IAR {
 				}
 				_a.txt = `Downloading the page (${c(d)}) ${parseInt(a / _icons.length * 100)}%.`
 			}
-			_a.txt = 'Building the Page'
-			this.update({UI: 1})
+			_a.txt = 'Connecting to the IC-Tech server.'
+			d = pram(location.search, 'eid')
+			if(!d) location = Host
+			XHR(API + 'verify?eid=' + d, a => this.update({UI: 1, st: a.success && a.response ? 0 : 1}))
 		}, defaultWait)
 	}
 	render() {
@@ -62,12 +65,15 @@ class SignIn extends IAR {
 				{ t: 'div', cl: ['ICPage', 'Main', 'c1'], s: {display: this.data.UI == 1 ? 'flex' : 'none'}, ch: [
 					{ t: 'div', ch: [
 						{t: 'div', cl: 'ico', html: icons ? icons[this.data.st] : null },
-						{t: 'span', txt: this.data.st == 0 ? `Your account have been activated.` : `Sorry, We can't complete your request. Please request for new link.`}
+						{t: 'span', cl: 'c3', txt: this.data.st == 0 ? `Your account have been activated.` : `Sorry, We can't complete your request. Please request for new link.`},
+						{t: 'div', cl: 'c1', ch: [
+							{t: 'a', cl: ['ic-btn0', 's1'], at: [['href', '/']], txt: 'IC-Tech'}
+						]}
 					]}
 				]}
 			]}
 		)
 	}
 }
-new SignIn().mount(_root_.v)
+new Verify().mount(_root_.v)
 }
