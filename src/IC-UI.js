f03a877dfffd1615797191a9491766a5c0ec8ccb
IC-Tech <ic.imesh.chamara@gmail.com>
2019-08-30 02:14:46 +0530

    Update IC-UI.js

--- src/IC-UI.js
+++ src/IC-UI.js
@@ -6,11 +6,14 @@ const icApp = ic.icApp
 var inputUI = a => (
 	{t: 'div', cl: 'inputui', s: a.s, ch: [
 		{t:'label', at: [['for', a.id]], txt: a.name},
-		{t: a.multi ? 'textarea' : 'input', at: [['readonly', a.readonly], ['type', a.type], ['type', a.id], ['id', a.id]], e: [['onfocus', inputUI.check], ['onblur', inputUI.check], ['onchange', inputUI.check], ['oninput', inputUI.check]]}
+		{t: a.multi ? 'textarea' : 'input', at: [['readonly', a.readonly], ['type', a.type], ['type', a.id], ['id', a.id]], e: [...[a.multi ? ['onkeyup', inputUI.resize ]: []], ['onfocus', inputUI.check], ['onblur', inputUI.check], ['onchange', inputUI.check], ['oninput', inputUI.check]]}
 ]})
 inputUI.check = a => new icApp.e(a = a.target).p[icApp.d.activeElement == a || a.value != '' ? 'cla' : 'clr']('s1')
 inputUI.checkAll = a => icApp.qsa('.inputui input').forEach(a=> inputUI.check({target: a}))
-
+inputUI.resize = e => {
+  (e = e.target).style.height = '2px'
+  e.style.height = 4 + e.scrollHeight + 'px'
+}
 var dialogUI = a => (
 	{t: 'div', cl: ['dialog-container', 'show'], at: [['id', 'ic-dialog-' + a.id]], ch: [
 		{t: 'div', cl: 'dialog', ch: [
