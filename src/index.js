7fdee1ee5d3d1bb70c51f5037c3bd27c5a24537b
IC-Tech <ic.imesh.chamara@gmail.com>
2019-09-06 22:55:48 +0530

    bug fix

--- src/index.js
+++ src/index.js
@@ -177,7 +177,6 @@ class ICTech extends IAR {
 		this.update()
 	}
 	render() {
-		const _a = {display: this.data.selfView ? 'block' : 'none'}
 		return (
 			{ t: 'div', cl: 'ICApp', ch: [
 				{ t: 'div', cl: ['ICPage', 'Main', 'c1'], s: {display: this.data.UI == 1 ? 'flex' : 'none'}, ch: [
@@ -196,11 +195,11 @@ class ICTech extends IAR {
 							]},
 							{ t: 'div', cl: 'c2', ch: [
 								{ t: 'a', at: [['href', '/']], html: icons[0] },
-								{ t: 'a', s: _a, e: [['onclick', a => this.update({UI: 2, st: 1})]], html: icons[3] },
+								{ t: 'a', s: {display: this.data.selfView ? 'block' : 'none'}, e: [['onclick', a => this.update({UI: 2, st: 1})]], html: icons[3] },
 								{ t: 'a', s: {display: this.data.selfView || !this.user ? 'none' : 'block'}, at: [['href', this.user ? '/?id='+this.user.id : '/']], html: icons[2] },
 								{ t: 'a', s: {display: this.user ? 'none' : 'block'}, at: [['href', '/signin.html']], html: icons[16] },
-								{ t: 'a', s: _a, e: [['onclick', this.deleteDialog]], html: icons[1] },
-								{ t: 'a', s: _a, e: [['onclick', this.logout]], html: icons[4] },
+								{ t: 'a', s: {display: this.data.selfView ? 'block' : 'none'}, e: [['onclick', this.deleteDialog]], html: icons[1] },
+								{ t: 'a', s: {display: this.data.selfView ? 'block' : 'none'}, e: [['onclick', this.logout]], html: icons[4] },
 								{ t: 'a', cl: this.darkTheme ? 's1' : 's0', e: [['onclick', e => {this.setTheme(!this.darkTheme)}]], html: icons[15] }
 							]}
 						]},
