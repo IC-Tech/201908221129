5a9b6c439e7cb91fb72e09163fcfa990e2760eb9
IC-Tech <ic.imesh.chamara@gmail.com>
2019-11-15 18:51:10 +0530

    Update reset.js

--- src/reset.js
+++ src/reset.js
@@ -10,6 +10,8 @@ import {inputUI} from './IC-UI.js'
 import {XHR, Host, API, IC_DEV, pram} from './common.js'
 import {ShowErr} from './error.js'
 
+window.ic = window.ic || []
+window.ic.pageLoad = Date.now()
 ic.init = icApp => {
 var _root_ = new icApp.e('#root')
 _root_.chr()
@@ -28,6 +30,7 @@ class Reset extends IAR {
 		this.submit = this.submit.bind(this)
 		this._a = (a => a + '=' + icApp.qs('#' + a).value).bind(this)
 		initTheme(icApp)
+		gtag('event', 'screen_view', { 'screen_name': 'Reset Password'})
 	}
 	didMount() {
 		var _a = new icApp.e('.load span')
@@ -35,37 +38,38 @@ class Reset extends IAR {
 		setTimeout(async a=> {
 			icons = []
 			_a.txt = 'Downloading the page.'
-			var b = a=> new Promise(r => XHR(Host + `assets/${a}.svg`, a => r(a), {raw:1}))
-			var c = a=> {
-				a = [a, 0]
-				while(a[0] >= 1024) {
-					a[1]++;
-					a[0] = a[0] / 1024
-				}
-				return parseInt(a[0]) + ([' bytes', 'KB', 'MB', 'GB'])[a[1]]
-			}
-			var d = 0
-			const _icons = [15, 13]
-			for(var a=0; a<_icons.length; a++) {
-				if(!(icons[a] = localStorage.getItem('ic-tech:assets:v0:icon' + _icons[a]))) {
-					icons[a] = await b(_icons[a])
-					d += icons[a].length
-					localStorage.setItem('ic-tech:assets:v0:icon' + _icons[a], icons[a])
-				}
-				_a.txt = `Downloading the page (${c(d)}) ${parseInt(a / _icons.length * 100)}%.`
-			}
+			_a.txt = 'Receiving the page.'
+			var b = JSON.parse(localStorage.getItem('ic-tech:assets:v1:reset'))
+			icons = !b || b == 'null' ? (await new Promise(r => XHR(Host + `assets/reset.json`, a => r(a))))['IC-Tech.Assets'] : b
+			if(!b || b == 'null') localStorage.setItem('ic-tech:assets:v1:reset', JSON.stringify(icons))
 			_a.txt = 'Connecting to the IC-Tech server.'
-			d = pram(location.search, 'eid')
-			if(!d) location = Host
-			XHR(API + 'can/reset?eid=' + d, a => {
-				this.update({UI:  a.success && a.response ? 2 : 1, st:1})
+			b = pram(location.search, 'eid')
+			if(!b) location = Host
+			XHR(API + 'can/reset?eid=' + b, a => {
+				this.update({UI:  (a = a.success && a.response) ? 2 : 1, st:1})
+				gtag('event', a ? 'Ready for Reset' : 'unable to reset with eid', {
+					event_category: 'Account',
+					event_label: 'Password reset'
+				})
 			})
+			;(['page_mount_end', 'Reset Page Load']).forEach(a => gtag('event', a, {
+  				'name': 'pageMount',
+  				'value': Date.now() - window.ic.pageLoad,
+  				'event_category': 'timing',
+  				'event_label': 'IC App'
+				}))
 		}, defaultWait)
 	}
 	submit(e) {
 		e.preventDefault()
 		this.update({UI: 0})
-		setTimeout(a => XHR(API + `reset?eid=${pram(location.search, 'eid')}&${this._a('password')}`, a=> this.update({UI: 1, st: a.success && a.response ? 0 : 1})), defaultWait)
+		setTimeout(a => XHR(API + `reset?eid=${pram(location.search, 'eid')}&${this._a('password')}`, a=> {
+			this.update({UI: 1, st: (a = a.success && a.response) ? 0 : 1})
+			gtag('event', a ? 'Done Reset' : 'exception', a ? ({
+				event_category: 'Account',
+				event_label: 'Password reset'
+			}) : ({'description': 'unable to reset password', 'fatal': true}))
+		}), defaultWait)
 		return false
 	}
 	didUpdate() {
@@ -81,7 +85,7 @@ class Reset extends IAR {
 				{ t: 'div', cl: ['ICPage', 'Res', 'c1'], s: {display: this.data.UI == 1 ? 'flex' : 'none'}, ch: [
 					{ t: 'div', ch: [
 						{t: 'div', cl: 'ico', html: icons ? icons[this.data.st] : null },
-						{t: 'span', cl: 'c3', txt: this.data.st == 0 ? `Your account have been activated.` : `Sorry, We can't complete your request. Please request for new link.`},
+						{t: 'span', cl: 'c3', txt: this.data.st == 0 ? `Your account password have been reseted.` : `Sorry, We can't complete your request. Please request for new link.`},
 						{t: 'div', cl: 'c1', ch: [
 							{t: 'a', cl: ['ic-btn0', 's1'], at: [['href', '/']], txt: 'IC-Tech'}
 						]}
@@ -90,7 +94,7 @@ class Reset extends IAR {
 				{ t: 'div', cl: ['ICPage', 'Main', 'c1'], s: {display: this.data.UI == 2 ? 'flex' : 'none'}, ch: [
 					{ t: 'div', ch: [
 						{ t: 'form', e: [['onsubmit', this.submit]], ch: [
-							{ t: 'span', cl: 'c1', txt: 'Reset Passwrod' },
+							{ t: 'span', cl: 'c1', txt: 'Reset Password' },
 							inputUI({id: 'password', type: 'password', name: 'Password', s: {display: this.data.state != 0 ? 'block' : 'none'} }),
 							{ t: 'span', cl: 'c2', txt: 'Please submit your new password for your account.' },
 							{ t: 'div', cl: 'c1', s: {paddingTop: '12px'}, ch: [
