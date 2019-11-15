2d524fb63d73078f726e64278ac13654ac3ddc63
IC-Tech <ic.imesh.chamara@gmail.com>
2019-11-15 18:51:02 +0530

    Update error.js

--- src/error.js
+++ src/error.js
@@ -4,7 +4,11 @@ import {Host} from './common.js'
 import {setUser} from './user.js'
 
 var ShowErr = (b,a) => {
-	if(b == 0) dialogUI.create({name: 'Server Error', msg: `Error detected in server. ${a ? '' : 'No '}Error information${a ? (b => [a.forEach((a,c) => b += (c == 0 ? '' : ', ') + a), b])(': ')[1] : ' Provided'}.`, but: ['RELOAD', 'CANCEL'], f: a=> {
+	gtag('event', 'exception', {
+		'description': (b >= 0 && b <= 2) ? (['Server Error ' + (a = (a ? (b => [a.forEach((a,c) => b += (c == 0 ? '' : ', ') + a), b])(': ')[1] : null)), 'email not verified', 'Update another'])[b] : 'unknown error',
+		'fatal': false
+	})
+	if(b == 0) dialogUI.create({name: 'Server Error', msg: `Error detected in server. ${a ? '' : 'No '}Error information${a ? a : ' Provided'}.`, but: ['RELOAD', 'CANCEL'], f: a=> {
 		if(a.b == 0) location.reload()
 		else window.close()
 	}})
@@ -15,7 +19,7 @@ var ShowErr = (b,a) => {
 	else if(b == 2) dialogUI.create({name: 'Account Security Update', msg: `Your account's security have been updated on another browser. You have to resign to the page. if you don't you can't use the App.`, but: ['OK', 'CANCEL'], f: a=> {
 		if(a.b == 0) {
 			setUser(null)
-			location.reload()
+			location = Host + 'signin.html'
 		}
 		else window.close()
 	}})
